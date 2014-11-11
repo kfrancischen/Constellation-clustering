1915968f5234bfcedf53dbe231d1228a762c8da4
kfrancischen <kfrancischen@gmail.com>
2014-11-10 22:48:54 -0800

    add comments

--- clustering.py
+++ clustering.py
@@ -7,11 +7,21 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-a','--algorithm',type = str, help = 'Choose the algorithm')
 algorithm = parser.add_subparsers(help = 'algorithm choices')
+
+'''
+	Running K means algorithm:
+	python cluster.py -a Kmeans k [the k value]
+'''
 kmeans_parser = algorithm.add_parser('k', help = 'number of clusters')
 kmeans_parser.add_argument('K', type = int, help = 'input the number of clusters')
+
+'''
+	Running DBSCAN:
+	python clustering.py -a DBSCAN eps [the eps value] mindist [the mindist value]
+'''
 eps_parser = algorithm.add_parser('eps',help = 'the epsilon value of a cluster')
 eps_parser.add_argument('Eps', type = float, help = 'the input epsilon')
-min_dist = eps_parser.add_subparsers(help = ' the minimum distance')
+min_dist = eps_parser.add_subparsers(help = 'the minimum distance')
 minDist_parser = min_dist.add_parser('mindist', help = 'minimum distance for a reachable point')
 minDist_parser.add_argument('minDist', type = int, help = 'the value of minimum distance')
 args = parser.parse_args()
@@ -26,6 +36,8 @@ starsWithName = dataProcessing.chooseStarWithName(database)
 
 # choosing the stars with brighness higher than 4.5
 starsNeedClustering = dataProcessing.selectBrightness(starsWithName, 4.5)
+
+# if the user runs kmeans
 if args.algorithm == 'Kmeans':
 	K = args.K
 	# running K means for 1000 times with 20 centroids
@@ -41,6 +53,8 @@ if args.algorithm == 'Kmeans':
 	visualization.visualize(standardKMeans.assignments)
 	#print len(assignments), len(cluster_1), cluster_1
 	# print centroids, assignments
+
+# if the user runs DBSCAN
 elif args.algorithm == 'DBSCAN':
 	Eps = args.Eps
 	minDist = args.minDist
