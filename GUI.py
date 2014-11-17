56ffaa8c71041450114b40075c4b5c09f91aa6f8
kfrancischen <kfrancischen@gmail.com>
2014-11-16 23:32:09 -0800

    change the gui

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'GUI.ui'
 #
-# Created: Sat Nov 15 22:10:59 2014
+# Created: Sun Nov 16 22:44:38 2014
 #      by: PyQt5 UI code generator 5.3.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -14,26 +14,17 @@ class Ui_Widget(object):
         Widget.setObjectName("Widget")
         Widget.setEnabled(True)
         Widget.setFixedSize(1113, 800)
-        self.resultView = QtWidgets.QGraphicsView(Widget)
-        self.resultView.setGeometry(QtCore.QRect(370, 60, 671, 611))
-        self.resultView.setObjectName("resultView")
         self.algorithmBox = QtWidgets.QComboBox(Widget)
         self.algorithmBox.setGeometry(QtCore.QRect(90, 110, 191, 51))
         self.algorithmBox.setObjectName("algorithmBox")
-        self.parametersView = QtWidgets.QTableView(Widget)
-        self.parametersView.setGeometry(QtCore.QRect(30, 180, 301, 491))
-        self.parametersView.setObjectName("parametersView")
         self.clearButton = QtWidgets.QPushButton(Widget)
-        self.clearButton.setGeometry(QtCore.QRect(640, 700, 161, 41))
+        self.clearButton.setGeometry(QtCore.QRect(860, 700, 161, 41))
         self.clearButton.setObjectName("clearButton")
-        self.viewButton = QtWidgets.QPushButton(Widget)
-        self.viewButton.setGeometry(QtCore.QRect(870, 700, 161, 41))
-        self.viewButton.setObjectName("viewButton")
         self.runButton = QtWidgets.QPushButton(Widget)
-        self.runButton.setGeometry(QtCore.QRect(90, 700, 161, 41))
+        self.runButton.setGeometry(QtCore.QRect(110, 700, 161, 41))
         self.runButton.setObjectName("runButton")
         self.saveButton = QtWidgets.QPushButton(Widget)
-        self.saveButton.setGeometry(QtCore.QRect(410, 700, 161, 41))
+        self.saveButton.setGeometry(QtCore.QRect(650, 700, 161, 41))
         self.saveButton.setObjectName("saveButton")
         self.algorithmLabel = QtWidgets.QLabel(Widget)
         self.algorithmLabel.setGeometry(QtCore.QRect(120, 70, 121, 26))
@@ -42,6 +33,26 @@ class Ui_Widget(object):
         font.setWeight(75)
         self.algorithmLabel.setFont(font)
         self.algorithmLabel.setObjectName("algorithmLabel")
+        self.parameterWidget = QtWidgets.QTableWidget(Widget)
+        self.parameterWidget.setGeometry(QtCore.QRect(50, 190, 256, 481))
+        self.parameterWidget.setObjectName("parameterWidget")
+        self.parameterWidget.setColumnCount(2)
+        self.parameterWidget.setRowCount(10)
+        self.scrollArea = QtWidgets.QScrollArea(Widget)
+        self.scrollArea.setGeometry(QtCore.QRect(400, 80, 631, 581))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 629, 579))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.clusteringResults = QtWidgets.QPlainTextEdit(self.scrollAreaWidgetContents)
+        self.clusteringResults.setGeometry(QtCore.QRect(0, 0, 629, 579))
+        self.clusteringResults.setObjectName("clusteringResults")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+	#self.scrollArea.setViewport(self.scrollAreaWidgetContents)
+        self.visualButton = QtWidgets.QPushButton(Widget)
+        self.visualButton.setGeometry(QtCore.QRect(420, 700, 161, 41))
+        self.visualButton.setObjectName("visualButton")
 
         self.retranslateUi(Widget)
         QtCore.QMetaObject.connectSlotsByName(Widget)
@@ -50,8 +61,8 @@ class Ui_Widget(object):
         _translate = QtCore.QCoreApplication.translate
         Widget.setWindowTitle(_translate("Widget", "Constellation Clustering"))
         self.clearButton.setText(_translate("Widget", "Clear"))
-        self.viewButton.setText(_translate("Widget", "View Clusters"))
         self.runButton.setText(_translate("Widget", "Run"))
         self.saveButton.setText(_translate("Widget", "Save"))
         self.algorithmLabel.setText(_translate("Widget", "Algorithms"))
+        self.visualButton.setText(_translate("Widget", "visualizing"))
 
